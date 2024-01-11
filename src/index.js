0363b7178dabcfcb3ea479941943eff79aad1639
Aprilliggons <149065998+Aprilliggons@users.noreply.github.com>
2024-01-11 18:36:18 -0500

    Add the API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ac33b3a348fbboc64004501e57ftfdb9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSumbit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearchSumbit);
